34d0a9a13db3b08c8827f2837802ca7817a8fe00
Brynley Llewellyn-Roux <blynblack@gmail.com>
2025-06-20 12:25:54 +1000

    chore: updated `Polykey-CLI`

--- packages.nix
+++ packages.nix
@@ -2,7 +2,7 @@
 
 let
   polykey-cli-flake = builtins.getFlake
-    "github:MatrixAI/Polykey-CLI/8bee0de0adedb64645a6ea48ddb63330c348b713";
+    "github:MatrixAI/Polykey-CLI/14f3fd31c42655cc3ddab6904cb6a3684cfb6f63";
 in lib.makeExtensible (self: {
   polykey-cli = polykey-cli-flake.packages.${system}.default;
   polykey-cli-docker = polykey-cli-flake.packages.${system}.docker;
